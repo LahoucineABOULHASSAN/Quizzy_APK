0aba9d694b16f8b20e9487dd2b1bf16424a85947
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2022-05-19 09:22:43 +0000

    quizz mother-board et cpu added

--- src/pages/QuizCPU.jsx
+++ src/pages/QuizCPU.jsx
@@ -4,27 +4,125 @@ const QuizCPU = () => {
       <h1 className="mb-4 p-1 pl-4 rounded-md font-medium leading-tight text-2xl text-orange-600">
         Quizz 3 (5pts):
       </h1>
-      <div className="bg-white">
-        <p>
-          mettre les mots convenable a leur place :mémoire, serveaux, traite,
-          stock, éxecute. (3)
-          <br />
-          le micro-processeur est le ....... d'un ordinateur.
-          <br />
-          le micro-processeur ....... les informations et ..... les commandes.
-          <br />
-          CPU est l'abréviation de ...... ? (1)
-          <br />
-          (central processing unit)
-          <br />
-          quel image coresspond au cpu? (1)
-          <br />
-          image 1<br />
-          image 2<br />
-          image 3<br />
-          aucune
-          <br />
-        </p>
+      <div className="">
+        <div className="flex justify-center my-4">
+          <ul className="bg-white rounded-lg border w-96 text-gray-900">
+            <li className="px-6 py-2 border-b w-full bg-gray-400 rounded-t-lg text-xl font-medium">
+              1- Mettre les mots convenable a leur place :{" "}
+              <strong>mémoire, serveaux, traite, stock, éxecute.</strong>{" "}
+            </li>
+            <li className="px-6 py-2 border-b w-full">
+              Le micro-processeur est le{" "}
+              <input
+                type="text"
+                className="w-16 form-control inline w-auto mx-2"
+                id="exampleFormControlInput4"
+                placeholder="................"
+              />{" "}
+              d'un ordinateur.
+            </li>
+            <li className="px-6 py-2 border-b w-full">
+              Le micro-processeur{" "}
+              <input
+                type="text"
+                className="w-16 form-control inline w-auto mx-2"
+                id="exampleFormControlInput4"
+                placeholder="................"
+              />{" "}
+              les informations et{" "}
+              <input
+                type="text"
+                className="w-16 form-control inline w-auto mx-2"
+                id="exampleFormControlInput4"
+                placeholder="................"
+              />{" "}
+              les commandes.
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-center my-4">
+          <ul className="bg-white rounded-lg border w-96 text-gray-900">
+            <li className="px-6 py-2 border-b w-full bg-gray-400 rounded-t-lg text-xl font-medium">
+              2- C'est quoi l'abréviation de CPU?{" "}
+            </li>
+            <li className="px-6 py-2 border-b w-full">
+              <input
+                type="text"
+                className="w-full form-control border border-solid border-gray-300 text-gray-700
+                bg-white bg-clip-padding px-4 py-1"
+                id="exampleFormControlInput4"
+                placeholder="................"
+              />
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-center my-4">
+          <ul className="bg-white rounded-lg border w-96 text-gray-900">
+            <li className="px-6 py-2 border-b w-full bg-gray-400 rounded-t-lg text-xl font-medium">
+              3- Quel image si-dessous correspond au cpu? :{" "}
+            </li>
+            <li className="px-6 py-2 border-b w-full">
+              <div className="form-check flex justify-center items-center flex-row">
+                <input
+                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  type="checkbox"
+                  value={0}
+                  id="flexCheckChecked"
+                />
+                <label
+                  className="form-check-label inline-block text-gray-800 flex flex-wrap justify-center"
+                  htmlFor="flexCheckChecked"
+                >
+                  <img
+                    src="/assets/cpu.png"
+                    className="w-52 h-auto shadow-lg"
+                    alt="cpu"
+                  />
+                </label>
+              </div>
+            </li>
+            <li className="px-6 py-2 border-b w-full">
+              <div className="form-check flex justify-center items-center flex-row">
+                <input
+                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  type="checkbox"
+                  value={0}
+                  id="flexCheckChecked"
+                />
+                <label
+                  className="form-check-label inline-block text-gray-800 flex flex-wrap justify-center"
+                  htmlFor="flexCheckChecked"
+                >
+                  <img
+                    src="/assets/gpu.png"
+                    className="w-52 h-auto shadow-lg"
+                    alt="carte graphique"
+                  />
+                </label>
+              </div>
+            </li>
+            <li className="px-6 py-2 border-b w-full">
+              <div className="form-check flex justify-center items-center flex-row">
+                <input
+                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  type="checkbox"
+                  value={0}
+                  id="flexCheckChecked"
+                />
+                <label
+                  className="form-check-label inline-block text-gray-800 flex flex-wrap justify-center"
+                  htmlFor="flexCheckChecked"
+                >
+                  <img
+                    src="/assets/mb.png"
+                    className="w-52 h-auto shadow-lg"
+                    alt="carte graphique"
+                  />
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
