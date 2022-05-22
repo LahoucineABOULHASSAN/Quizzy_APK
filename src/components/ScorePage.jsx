220e2900da8510429e045ea924a78715fce64bf6
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2022-05-22 15:20:44 +0000

    remove bye btn

--- src/components/ScorePage.jsx
+++ src/components/ScorePage.jsx
@@ -19,15 +19,6 @@ const ScorePage = ({ score, totale }) => {
         <h3 class="text-3xl font-bold mb-8">
           Vous avez arriver au niveau {niveau}{" "}
         </h3>
-        <a
-          class="inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-          href="#!"
-          role="button"
-        >
-          Aurevoir
-        </a>
       </div>
 
       <div
